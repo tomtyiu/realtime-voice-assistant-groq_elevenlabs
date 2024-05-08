3263d7a2f969df4ed76dab63e578a3a6242c3a13
Thomas YIu <tom.tyiu@gmail.com>
2024-05-08 14:51:00 -0700

    Update route.ts

--- app/api/neets/route.ts
+++ app/api/neets/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server';
 
 export const GET = async () => {
-    const apiKey = process.env.NEETS_API_KEY
+    const apiKey = process.env.ELEVENLABS_API_KEY
     return NextResponse.json({ apiKey: apiKey }, { status: 200 });
 };
